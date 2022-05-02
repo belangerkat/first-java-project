48256a6b91f20c5f59b7ea5b90c0829019ddfe9f
belangerkat <belangerkt@gmail.com>
2022-05-02 15:01:26 -0700

    Added Frameworks Class

---
+++ SecurityFrameworks.java
@@ -0,0 +1,6 @@
+public class SecurityFrameworks {
+    public static void main (String[] args) {
+        String[] frameworks = {"PCI DSS", "HITRUST", "CIS v8", "HIPAA"};
+        System.out.println(frameworks[2]);
+    }
+}
