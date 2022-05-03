b9a92056bdc7030b23d5d023f7b191b988ac389f
belangerkat <belangerkt@gmail.com>
2022-05-03 13:03:57 -0700

    for loop

--- SecurityFrameworks.java
+++ SecurityFrameworks.java
@@ -2,5 +2,8 @@ public class SecurityFrameworks {
     public static void main (String[] args) {
         String[] frameworks = {"PCI DSS", "HITRUST", "CIS v8", "HIPAA"};
         System.out.println(frameworks[2]);
+        for(int x = 3; x < 15; x = x + 2){
+            System.out.println("Under 15");
+        }
     }
 }
